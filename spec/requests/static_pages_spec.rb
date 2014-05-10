2a1f72375f11df9e6c4af0672fc55f3e562339f3
hidehigo <akihide.higo@gaiax.com>
2014-05-11 07:49:29 +0900

    set title for static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,6 +6,11 @@ describe "StaticPages" do
       visit 'static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit 'static_pages/home'
+      expect(page).to have_title('Home')
+    end
   end
 
   describe "Help page" do
@@ -13,12 +18,22 @@ describe "StaticPages" do
       visit 'static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit 'static_pages/help'
+      expect(page).to have_title('Help')
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit 'static_pages/about'
       expect(page).to have_content('About Us')
+    end
+
+    it "should have the title 'Home'" do
+      visit 'static_pages/about'
+      expect(page).to have_title('About')
     end
   end
 end
