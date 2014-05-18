f7a58edf2537e481298e26f2087a8ec97dcc9852
hidehigo <akihide.higo@gaiax.com>
2014-05-18 23:17:35 +0900

    refactor test: to use before

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,18 +5,16 @@ describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
+    before { visit root_path }
     it "should have the content 'Sample App'" do
-      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should have the title 'Home'" do
-      visit root_path
       expect(page).not_to have_title("| Home")
     end
   end
