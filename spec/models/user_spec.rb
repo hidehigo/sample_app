b70f9bc6668f3919f2f5c13076d728000df5adaa
hidehigo <akihide.higo@gaiax.com>
2014-05-25 09:49:21 +0900

    add password_confirmation and use has_secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,15 @@
 require 'spec_helper'
 
 describe User do
-	before { @user = User.new(name: "Example User", email: "user@example.com") }
+	before { @user = User.new(name: "Example User", email: "user@example.com",
+													  password: "foobar", password_confirmation: "foobar") }
 	subject { @user }
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 	
 	it { should be_valid }
 
@@ -19,6 +22,14 @@ describe User do
 		before { @user.email = " " }
 		it { should_not be_valid }
 	end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
 
 	describe "when name is too long" do
 		before { @user.name = "a" * 51 }
@@ -56,5 +67,8 @@ describe User do
     it { should_not be_valid }
   end
 
-
+  describe "when password doesn't match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
 end
